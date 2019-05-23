9679022921232aa5aeab6d0c012f8da359311458
Justin Paterson <jpaterson06@gmail.com>
2019-05-22 21:37:30 -0400

    Add unit tests and report to coveralls (#4)
    * Remove typescript
    
    * Add health check
    
    * Add style
    
    * Add unit tests
    
    * Add snyk
    
    * Add unit test reporting
    
    * Remove nyc
    
    * report unit tests
    
    * exec from node_modules
    
    * run
    
    * Names
    
    * install
    
    * cache

---
+++ tests/routes/health.test.js
@@ -0,0 +1,42 @@
+const request = require('supertest');
+
+let req = {
+    body: {}
+};
+
+let res = {
+    sendCalledWith: '',
+    send: function (arg) {
+        this.sendCalledWith(arg);
+    }
+};
+
+describe('Health Route', function () {
+    describe('GET api/health', function () {
+
+        var server;
+
+        beforeEach(function () {
+            delete require.cache[require.resolve('../../bin/www')];
+            server = require('../../bin/www', { bustCache: true });
+        });
+
+        afterEach(function (done) {
+            server.close(done);
+        })
+    
+        it ('Should return a http status code of 200', function (done) {
+            request(server)
+                .get('/api/health')
+                .expect(200, done);
+        });
+
+        it ('Should return status of "pass"', function (done) {
+            request(server)
+                .get('/api/health')
+                .expect(200, {
+                    status: 'pass'
+                }, done);
+        });
+    });
+});
