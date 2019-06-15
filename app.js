79f5ccdfd243714304d87db3658290467922198e
Justin Paterson <jpaterson06@gmail.com>
2019-06-14 21:14:17 -0400

    Add auth0

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const jwt = require('express-jwt');
+const jwks = require('jwks-rsa');
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error: '));
@@ -15,6 +18,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+
+var jwtCheck = jwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: process.env.AUTH_JWKSURI
+    }),
+    audience: process.env.AUTH_AUDIENCE,
+    issuer: process.env.AUTH_ISSUER,
+    algorithms: ['RS256']
+});
+
+app.use(jwtCheck);
 
 app.use('/api/health', require('./api/health/health.route'));
 app.use('/api/recipes', require('./api/recipes/recipes.route'));
