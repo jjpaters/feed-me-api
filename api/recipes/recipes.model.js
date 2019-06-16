cef260f1040811163a8aee62b7d7d85e555fe172
Justin Paterson <jpaterson06@gmail.com>
2019-06-15 20:07:49 -0400

    Update model with id

--- api/recipes/recipes.model.js
+++ api/recipes/recipes.model.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-var RecipeSchema = new Schema({
+const RecipeSchema = new Schema({
     title: { type: String, required: true },
     description: { type: String, required: true },
     userId: { type: String, required: true },
     updated: { type: Date, default: Date.now() },
+});
+
+RecipeSchema.set('toJSON', {
+    virtuals: true
 });
 
 module.exports =  mongoose.model('Recipe', RecipeSchema);
