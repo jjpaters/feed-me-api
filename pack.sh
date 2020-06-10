feaaa45d07e12d0c8c4ed35063fa09a1b1d899f6
Justin Paterson <jpaterson06@gmail.com>
2020-06-09 21:18:43 -0400

    Convert to botnet lambda API (#23)
    * Switch to dotnet
    
    * Create zip
    
    * Test zip
    
    * Remove master filter
    
    * Add zip
    
    * Add zip
    
    * Deploy step
    
    * Add master protection
    
    * Enable CORS

--- pack.sh
+++
@@ -1,7 +0,0 @@
-rm -r dist
-npm run build
-cp package.json ./dist/package.json
-cp package-lock.json ./dist/package-lock.json
-cd ./dist/ && npm install --production
-zip -r function.zip .
-cp function.zip ../function.zip
--- src/models/api-response.ts
+++
@@ -1,6 +0,0 @@
-import { HttpStatusCodes } from './http-status-codes';
-
-export class ApiResponse {
-    body: any;
-    statusCode?: HttpStatusCodes;
-}
