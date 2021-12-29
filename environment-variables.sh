bfda0c55ea61c9fd7327ca2a3128f59f3c9ce994
jpaterson <JustinPaterson@quickenloans.com>
2021-12-29 14:52:15 -0500

    Replace variables

---
+++ environment-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Global Variables
+FILEPATH				=	"./dist"
+
+# AWS Variables
+AWS_REGION				=	${AWS_REGION}
+AWS_USERPOOL_CLIENT_ID	=	${AWS_USERPOOL_CLIENT_ID}
+AWS_USERPOOL_ID			=	${AWS_USERPOOL_ID}
+
+echo "Starting to replace the AWS environment variables."
+
+sed -i -e "s,\#{AWS_REGION}\#,"AWS_REGION",g" -e "s,\#{AWS_USERPOOL_CLIENT_ID}\#,"AWS_USERPOOL_CLIENT_ID",g" -e "s,\#{AWS_USERPOOL_ID}\#,"AWS_USERPOOL_ID",g" $FILEPATH/appsettings.json
