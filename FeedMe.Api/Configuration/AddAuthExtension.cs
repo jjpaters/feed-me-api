8373b31b24b3e938051c27e4a28f0bd3b81a56f3
Justin Paterson <JustinPaterson@rocketmortgage.com>
2022-09-22 11:14:37 -0400

    Fix env vars

--- FeedMe.Api/Configuration/AddAuthExtension.cs
+++ FeedMe.Api/Configuration/AddAuthExtension.cs
@@ -1,30 +1,1 @@
-﻿using System.Security.Claims;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.IdentityModel.Tokens;
-
-namespace FeedMe.Api.Configuration
-{
-    public static class AddAuthExtension
-    {
-        public static IServiceCollection AddCustomAuth(this IServiceCollection services, IConfiguration config)
-        {
-            services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            }).AddJwtBearer(options =>
-            {
-                options.Authority = config["Auth:Authority"];
-                options.Audience = config["Auth:Audience"];
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    NameClaimType = ClaimTypes.NameIdentifier
-                };
-            });
-
-            return services;
-        }
-    }
-}
+﻿using System.Security.Claims;using Microsoft.AspNetCore.Authentication.JwtBearer;using Microsoft.Extensions.Configuration;using Microsoft.Extensions.DependencyInjection;using Microsoft.IdentityModel.Tokens;namespace FeedMe.Api.Configuration{    public static class AddAuthExtension    {        public static IServiceCollection AddCustomAuth(this IServiceCollection services, IConfiguration config)        {            services.AddAuthentication(options =>            {                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;            }).AddJwtBearer(options =>            {                options.Authority = config["Auth:Authority"];                options.Audience = config["Auth:Audience"];                options.TokenValidationParameters = new TokenValidationParameters                {                    NameClaimType = ClaimTypes.NameIdentifier                };            });            return services;        }    }}
