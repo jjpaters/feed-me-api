63e0e0dca64ce7ecb5cda3b3f9ca3c2f475dfada
Justin Paterson <JustinPaterson@rocketmortgage.com>
2022-09-27 17:02:06 -0400

    Fix env vars for prod

--- environment-variables.sh
+++ environment-variables.sh
@@ -11,7 +11,8 @@ DYNAMO_SECRET_KEY=${DYNAMO_SECRET_KEY}
 
 echo "Starting to replace the AWS environment variables."
 
-sed -i -e "s,\#{AUTH_AUTHORITY}\#,"AUTH_AUTHORITY",g"\
-    -e "s,\#{DYNAMO_ACCESS_KEY}\#,"DYNAMO_ACCESS_KEY",g"\
-    -e "s,\#{DYNAMO_SECRET_KEY}\#,"DYNAMO_SECRET_KEY",g"\
+sed -i -e "s,\#{AUTH_AUTHORITY}\#,"$AUTH_AUTHORITY",g"\
+    -e "s,\#{AUTH_AUDIENCE}\#,"$AUTH_AUDIENCE",g"\
+    -e "s,\#{DYNAMO_ACCESS_KEY}\#,"$DYNAMO_ACCESS_KEY",g"\
+    -e "s,\#{DYNAMO_SECRET_KEY}\#,"$DYNAMO_SECRET_KEY",g"\
     $FILEPATH
